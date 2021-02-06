edf89e6ce12842c2f5b6df0d8d6f655c4ae091a2
msft-jasonparker <jason.parker@microsoft.com>
2021-02-06 11:00:20 -0700

    added show menu function and updated readme.md

--- Enable-LocalNetworkCapture.ps1
+++ Enable-LocalNetworkCapture.ps1
@@ -1,12 +1,90 @@
-﻿[CmdletBinding()]
+﻿<#
+.SYNOPSIS
+    Configures a local computer to capture network traces
+.DESCRIPTION
+    This script will download and install the (deprecated) Microsoft Network Monitor 3.4 application. Once installed, a scheduled task will be created to capture network traffic anytime a user is logged on to the computer. The captures files will rollover to a new file every 250MB. A separate task will pruned the logging directory for capture files older than 3 days. The solution files can be found in C:\Windows\Utilities and the tasks are created in Microsoft > Windows > NetTracing.
+.EXAMPLE
+    PS C:\> Enable-LocalNetworkCapture.ps1
+    Single script operation will check and validate the computer is configured for network captures.
+.NOTES
+    Requires RunAs Administrator
+#>
+[CmdletBinding()]
 Param ()
     BEGIN {
         #Requires -RunAsAdministrator
+        Function Show-Menu {
+            <#
+                .SYNOPSIS
+                    Shows a console based menu and title or just a console title banner in a variety of colors and stles.
+                .DESCRIPTION
+                    Create a console based menu and use this function to display it with a descriptive title bar. This function is primarily used to display the title bar in a variety of colors and sytles. It is useful when used to convey important information to the console operator.
+            #>
+            Param (
+                # Single line title or banner used as a desciption or message to the console operator
+                [Parameter(Position=0,Mandatory=$true)]
+                [System.String]$Title,
+        
+                # Console based menu with multiple selection options
+                [Parameter(Position=1,Mandatory=$false)]
+                [System.String]$Menu,
+        
+                # Allows for a variety of style selections and sizes, default style (full)
+                [Parameter(Position=2,Mandatory=$false)]
+                [ValidateSet("Full","Mini","Info")]
+                [System.String]$Style = "Full",
+        
+                # Foreground text colors
+                [Parameter(Position=3,Mandatory=$false)]
+                [ValidateSet("White","Cyan","DarkCyan","Magenta","Yellow","DarkYellow","Green","DarkGreen","Red","DarkRed","Gray","DarkGray","Blue","DarkBlue")]
+                [System.String]$Color = "Gray",
+        
+                # Clears the console screen before displaying the menu / title
+                [Parameter(Position=4,Mandatory=$false)]
+                [Switch]$ClearScreen,
+        
+                # Does not prompt for menu selection, shows the menu display only.
+                [Parameter(Position=5,Mandatory=$false)]
+                [Switch]$DisplayOnly
+            )
+        
+            [System.Text.StringBuilder]$menuPrompt = ""
+            Switch($Style) {
+                "Full" {
+                    [Void]$menuPrompt.AppendLine("/" * (95))
+                    [Void]$menuPrompt.AppendLine("////`n`r//// $Title`n`r////")
+                    [Void]$menuPrompt.AppendLine("/" * (95))
+                }
+                "Mini" {
+                    [Void]$menuPrompt.AppendLine("\" * (80))
+                    [Void]$menuPrompt.AppendLine(" $Title")
+                    [Void]$menuPrompt.AppendLine("\" * (80))
+                }
+                "Info" {
+                    [Void]$menuPrompt.AppendLine("-" * (80))
+                    [Void]$menuPrompt.AppendLine("-- $Title")
+                    [Void]$menuPrompt.AppendLine("-" * (80))
+                }
+            }
+        
+            #add the menu
+            If (-NOT [System.String]::IsNullOrEmpty($Menu)) { [Void]$menuPrompt.Append($Menu) }
+            If ($ClearScreen) { [System.Console]::Clear() }
+            If ($DisplayOnly) {Write-Host $menuPrompt.ToString() -ForegroundColor $Color}
+            Else {
+                [System.Console]::ForegroundColor = $Color
+                Read-Host -Prompt $menuPrompt.ToString()
+                [System.Console]::ResetColor()
+            }    
+        }
+
         $CurrentDirectory = Get-Location
         $TempPath = ("{0}\NetMon" -f $env:TEMP)
         $NetMonDownload = ("{0}\NM34_x64.exe" -f $TempPath)
     }
     PROCESS {
+        Show-Menu -Title "Install / Enable Local Network Monitoring" -Style Full -DisplayOnly -ClearScreen -Color White
+        Write-Host ("Checking if Network Monitor is installed") -ForegroundColor Cyan
         If (Test-Path -Path "C:\Program Files\Microsoft Network Monitor 3\nmcap.exe") { Write-Host ("Network Monitor is installed") -ForegroundColor Green }
         Else {
             Write-Host ("Downloading Microsoft Network Monitor 3.4") -ForegroundColor Cyan
@@ -64,4 +142,7 @@ Param ()
         }
         Else { Write-Host ("NetMonCleanUp Scheduled Task already exists") -ForegroundColor Green }
     }
-    END { Set-Location $CurrentDirectory }
+    END {
+        Get-ChildItem -Name null | Remove-Item -Force
+        Set-Location $CurrentDirectory
+    }
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# NetMonTracing
-Scripts and configuration files used to download, install, and automate Microsoft Network Monitor v3.4 tracing on a local computer
+# Enable Local Network Capture
+
+> *Based on Microsoft Network Monitor 3.4 (deprecated)*
+## Overview
+
+There are cases when troubleshooting network related issues where having a local network packet capture is useful. This is especially true when the issue you are troubleshooting is intermittent. Microsoft Network Monitor 3.4, while deprecated and no longer updated, is easy to install, use and configure for automated capturing.
+
+## Contents
+
+Item  |   Description
+--- |   ---
+Enable-LocalNetworkCapture.ps1  |   This is a single script which will download and install the application and configure the scheduled tasks
+NetMonCapture.xml   |   XML file used to create a scheduled task for the packet capture
+NetMonCleanUp.xml   |   XML file used to create a scheduled task to run the NetMonCleanUp.ps1 script (prunes the old capture files)
+NetMonCleanUp.ps1   |   Script used to clean up capture files older than 3 days
+
+## Getting Started
+
+This is a very easy and straight forward solution. Follow the steps below:
+
+- Clone this repository or download and extract the zip file
+- Open PowerShell as an Administrator
+- Navigate to the directory where the Enable-LocalNetworkCapture.ps1 file is located
+- Run the script!
+
+> **NOTE:**
+> 
+> *If this is the first time running a script from the Internet, you'll need to update your execution policy*
+>
+> ``` Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope CurrentUser ```
